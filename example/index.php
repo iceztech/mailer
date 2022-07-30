f207b7b160a2f88b1b3bccfa78f493a73c29fd59
ZIKZAY <zikzay@yahoo.com>
2022-07-30 14:58:41 +0100

    Add example

--- example/index.php
+++ example/index.php
@@ -11,5 +11,8 @@ $subject = 'Test Subject';
 $body = '<h1>Title</h1><p>The body of the message of the email</p>';
 $to = 'zikzay@gmail.com';
 
-$send = Mailer::sendMail($host, $username, $password, 'Text Mailer', ['to' => $to, 'subject' => $subject, 'message' => $body]);
-var_dump($send);
+// $send = Mailer::sendMail($host, $username, $password, 'Text Mailer', ['to' => $to, 'subject' => $subject, 'message' => $body]);
+// var_dump($send);
+
+$mail = new Mailer($username, $password, 'Iceztech', $host);
+$mail->send($to, $subject, $message);
